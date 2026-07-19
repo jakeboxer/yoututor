8e19e71612e1a10b2e5cc893ff80cd058f8c9a19
Jake Card <jacob.boxer@gmail.com>
2026-07-19 15:29:43 -0700

    Preserve blank lines when rendering markdown

--- src/console/log-line-view.tsx
+++ src/console/log-line-view.tsx
@@ -10,10 +10,43 @@ export type LogLine = {
 	text: string;
 };
 
+function renderReply(markdown: string): string {
+	const blocks: string[] = [];
+	let current: string[] = [];
+	let inCodeFence = false;
+
+	for (const line of markdown.split("\n")) {
+		// Keep track of whether or not we're in a code fence.
+		if (/^\s*(```|~~~)/.test(line)) {
+			inCodeFence = !inCodeFence;
+		}
+
+		if (!inCodeFence && line.trim() === "") {
+			// If we hit an empty line (and we're not in a code fence), add the current block to the list
+			// of blocks and start a new one.
+			if (current.length) {
+				blocks.push(current.join("\n"));
+			}
+
+			current = [];
+		} else {
+			// Add this line to the current block we're building up.
+			current.push(line);
+		}
+	}
+
+	if (current.length) {
+		blocks.push(current.join("\n"));
+	}
+
+	// Join the blocks into a single string. Each block is separated by a blank line.
+	return blocks.map((b) => renderMarkdown(b, { wrap: false }).trim()).join("\n\n");
+}
+
 export default function LogLineView(props: { line: LogLine }) {
 	switch (props.line.kind) {
 		case "reply":
-			return <Text>{renderMarkdown(props.line.text, { wrap: false }).trim()}</Text>;
+			return <Text>{renderReply(props.line.text)}</Text>;
 		case "toolStart":
 			return <Text color="yellow">{props.line.text}</Text>;
 		case "toolDone":
